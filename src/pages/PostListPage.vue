9eb42a9df37c0fed7ff7fc7ae8da875e65bc651a
hsw <kswhgg@naver.com>
2022-05-14 21:16:25 +0900

    커뮤니티 게시글 읽기 기능

---
+++ src/pages/PostListPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post-list-page">
+    <h1>포스트 게시글</h1>
+    <post-list :posts="posts"/>
+  </div>
+</template>
+<script>
+import PostList from '@/components/PostList'
+import api from '@/api'
+
+export default {
+  name: 'PostListPage',
+  data() {
+      return {
+          posts: []
+      }
+  },
+  created() {
+      api.get('/posts')
+      .then(res => {
+          this.posts = res.data
+      })
+  },
+  components: {PostList},
+}
+</script>
