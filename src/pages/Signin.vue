36615a38929b015ed94b46640bad42f133e049be
hsw <kswhgg@naver.com>
2022-05-15 21:11:28 +0900

    로그인 페이지

---
+++ src/pages/Signin.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="sign-in-page">
+  <h3>로그인</h3>
+  <signin-form @submit="onSubmit"/>
+  <p>회원이 아니신가요? <router-link :to="{ name: 'Signup' }">회원가입 하러가기</router-link></p>
+</div>
+</template>
+
+<script>
+import SigninForm from '@/components/SigninForm'
+export default {
+  name: 'Signin',
+  methods: {
+    onSubmit (payload) {
+        console.log('payload -> ', payload)
+    },
+  },
+  components: {
+    SigninForm
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Signup from '@/pages/Signup'
+import Signin from '@/pages/Signin'
 import PostListPage from '@/pages/PostListPage'
 import PostViewPage from '@/pages/PostViewPage'
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/signup',
       name: 'Signup',
       component: Signup
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
     },
     {
       path: '/post/:postId',
